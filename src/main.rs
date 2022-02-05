e1ae8f0579200965f21b722aeb405785410a1ba1
Isitha Subasinghe <isubasinghe@student.unimelb.edu.au>
2022-02-05 17:42:05 +1100

    implemend PaxosConfig

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use serde::{Deserialize, Serialize};
 use stateright::actor::{register::*, *};
-use stateright::semantics::LinearizabilityTester;
 use stateright::semantics::register::Register;
+use stateright::semantics::LinearizabilityTester;
 use std::borrow::Cow;
 use std::collections::{BTreeMap, BTreeSet};
 use std::net::{Ipv4Addr, SocketAddrV4};
@@ -26,7 +26,6 @@ impl PaxosState {
         }
     }
 }
-
 
 // strategy to make forward progress on Paxos
 // "majority wins" is not needed for linearizability only for the strict (arguably correct) definition of "consensus".
@@ -109,7 +108,7 @@ impl Actor for PaxosActor {
         match msg {
             RegisterMsg::Internal(internal_msg) => {
                 match internal_msg {
-                    // request_id is stateright specific while rid is the round identifier 
+                    // request_id is stateright specific while rid is the round identifier
                     PaxosMsg::Prepare(request_id, rid) => {
                         if rid > state.last_seen {
                             let state = state.to_mut();
@@ -121,7 +120,7 @@ impl Actor for PaxosActor {
                         }
                     }
 
-                    // request_id is stateright specific while rid is the round identifier 
+                    // request_id is stateright specific while rid is the round identifier
                     PaxosMsg::Promise(request_id, rid) => {
                         let state = state.to_mut();
                         match state.promises.get_mut(&rid) {
@@ -147,13 +146,15 @@ impl Actor for PaxosActor {
                         let num_peers = self.peers.len();
                         // we have a majority
                         if count / 2 > num_peers {
-                            let msg = RegisterMsg::Internal(PaxosMsg::Accept(request_id, rid, value));
+                            let msg =
+                                RegisterMsg::Internal(PaxosMsg::Accept(request_id, rid, value));
                             o.send(src, msg);
                         }
                     }
                     PaxosMsg::Accept(request_id, rid, value) => {
                         if Some(rid) == state.last_seen {
-                            let msg = RegisterMsg::Internal(PaxosMsg::Accepted(request_id, rid, value));
+                            let msg =
+                                RegisterMsg::Internal(PaxosMsg::Accepted(request_id, rid, value));
                             o.broadcast(&self.peers, &msg);
                         }
                     }
@@ -178,8 +179,7 @@ impl Actor for PaxosActor {
 
                         let num_peers = self.peers.len();
                         if count / 2 > num_peers {
-                            // let msg = RegisterMsg::PutOk(rid);   
-                            
+                            // let msg = RegisterMsg::PutOk(rid);
                         }
                     }
                 }
@@ -190,7 +190,7 @@ impl Actor for PaxosActor {
                 state.prepare_data.insert(rid, value);
                 let msg = RegisterMsg::Internal(PaxosMsg::Prepare(request_id, state.next_round()));
                 o.broadcast(&self.peers, &msg);
-            }, 
+            }
             _ => {}
         }
     }
@@ -199,25 +199,64 @@ impl Actor for PaxosActor {
 #[derive(Clone)]
 struct PaxosModelConfig {
     client_count: usize,
-    server_count: usize
+    server_count: usize,
 }
 
 impl PaxosModelConfig {
-
-    fn into_model(self) {
-        todo!()
+    fn into_model(
+        self,
+    ) -> ActorModel<
+        RegisterActor<PaxosActor>,
+        Self,
+        LinearizabilityTester<Id, Register<RegisterValue>>,
+    > {
+        ActorModel::new(
+            self.clone(),
+            LinearizabilityTester::new(Register(RegisterValue::default())),
+        )
+        .actors((0..self.server_count).map(|i| {
+            RegisterActor::Server(PaxosActor {
+                peers: model_peers(i, self.server_count),
+            })
+        }))
+        .actors((0..self.client_count).map(|i| RegisterActor::Client {
+            put_count: 1,
+            server_count: self.server_count,
+        }))
+        .duplicating_network(DuplicatingNetwork::No)
+        .property(
+            stateright::Expectation::Always,
+            "linearizable",
+            |_, state| state.history.serialized_history().is_some(),
+        )
+        .property(
+            stateright::Expectation::Sometimes,
+            "value chosen",
+            |_, state| {
+                for env in &state.network {
+                    if let RegisterMsg::GetOk(_, value) = env.msg {
+                        if value != RegisterValue::default() {
+                            return true;
+                        }
+                    }
+                }
+                false
+            },
+        )
+        .record_msg_in(RegisterMsg::record_returns)
+        .record_msg_out(RegisterMsg::record_invocations)
     }
 }
-
+/*
 #[cfg(test)]
 mod test {
     use super::*;
     use stateright::{semantics::register::*, semantics::*, *};
     use ActorModelAction::Deliver;
     use RegisterMsg::{Get, GetOk, Put, PutOk};
-
 }
-
+ */
 fn main() {
-    println!("Hello, world!");
+    let address = "localhost:3000";  
+    println!("Serving from {0}", address);
 }
